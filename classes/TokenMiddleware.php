f777fc1d559ad1ac8417d7ad4f8df5aab2c4f18d
Sascha Aeppli <sascha.aeppli@liip.ch>
2021-04-22 13:40:33 +0200

    fire event in token middleware to prevent banned or inactive user from access.

--- classes/TokenMiddleware.php
+++ classes/TokenMiddleware.php
@@ -5,6 +5,7 @@ namespace Liip\User\Classes;
 use Closure;
 use RainLab\User\Facades\Auth;
 use RainLab\User\Models\User;
+use Event;
 
 class TokenMiddleware
 {
@@ -14,6 +15,9 @@ class TokenMiddleware
         if (!empty($token)) {
             $user = User::where('api_token', $token)->first();
             if ($user != null) {
+                if ($message = Event::fire('liip.user.authenticated', [$user], true)) {
+                    return response($message, 403);
+                }
                 Auth::setUser($user);
             }
         }
