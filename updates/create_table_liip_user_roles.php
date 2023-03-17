53c3d1f331992a8104738c2666bcc9542627963f
Juerg Hunziker <juerg.hunziker@liip.ch>
2023-03-17 17:11:44 +0100

    rename migrations

---
+++ updates/create_table_liip_user_roles.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Liip\User\Updates;
+
+use October\Rain\Database\Schema\Blueprint;
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class CreateTableLiipUserRoles extends Migration
+{
+    public function up()
+    {
+        Schema::create('liip_user_roles', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('name')->unique('liip_user_role_unique');
+            $table->string('code')->nullable()->index('liip_user_role_code_index');
+            $table->text('description')->nullable();
+            $table->text('permissions')->nullable();
+            $table->timestamps();
+        });
+        if (Schema::hasTable('users')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->integer('role_id')->nullable();
+            });
+        }
+    }
+
+    public function down()
+    {
+        if (Schema::hasTable('users')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->dropColumn('role_id');
+            });
+        }
+        Schema::drop('liip_user_roles');
+    }
+}
